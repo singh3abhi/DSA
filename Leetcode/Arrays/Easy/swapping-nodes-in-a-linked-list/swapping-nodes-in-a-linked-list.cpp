b82095ed7d670f1a1827e874a3946bda2f26cb65
singh3abhi <99040717+singh3abhi@users.noreply.github.com>
2023-05-16 02:09:31 +0530

    Time: 718 ms (18.14%) | Memory: 180.2 MB (16.23%) - LeetSync

--- Leetcode/Arrays/Easy/swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ Leetcode/Arrays/Easy/swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -11,23 +11,19 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* temp = head;
-        ListNode* right_ptr = head;
-        ListNode* left_ptr = NULL;
-
-        for(int i = 0; i < k-1; i++){
-            temp = temp->next;
-        }
-
-        left_ptr = temp;
-
-        while(temp->next != NULL){
-            temp = temp->next;
+        ListNode *left_ptr = head, *right_ptr = head;
+        for (int i = 0; i < k-1; i++) {
             right_ptr = right_ptr->next;
         }
-
-        swap(left_ptr->val,right_ptr->val);
-
+        
+        ListNode *end_ptr = right_ptr;
+        while (right_ptr->next) {
+            left_ptr = left_ptr->next;
+            right_ptr = right_ptr->next;
+        }
+        
+        swap(end_ptr->val, left_ptr->val);
+        
         return head;
     }
 };
